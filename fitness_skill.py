3ffe2f55e56f6f8cd11419bb266ada808655c588
Palmer Paul <pzpaul2002@yahoo.com>
2017-01-21 19:21:57 -0500

    store workout information using session

--- fitness_skill.py
+++ fitness_skill.py
@@ -1,11 +1,11 @@
+import json
 import logging
 
 from flask import Flask
 from flask import render_template
-
 from flask_ask import Ask
 # from flask_ask import request
-# from flask_ask import session
+from flask_ask import session
 from flask_ask import question
 from flask_ask import statement
 
@@ -24,12 +24,16 @@ def launch():
 @ask.intent('StartWorkoutIntent')
 def hello_world():
     speech_text = render_template('start')
+    default_workouts = json.load('defaultWorkouts.json')
+    session.attributes['workout'] = default_workouts['default']
+    session.attributes['activity_number'] = 0
     return statement(speech_text).simple_card('HelloWorld', speech_text)
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    return launch()
+    speech_text = render_template('welcome')
+    return question(speech_text).reprompt(speech_text)
 
 
 @ask.session_ended
