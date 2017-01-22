daf7f6771d1241e479bb85b1322c28bdcae3af4d
Palmer Paul <pzpaul2002@yahoo.com>
2017-01-21 19:15:31 -0500

    create StartWorkoutIntent

--- fitness_skill.py
+++ fitness_skill.py
@@ -1,7 +1,13 @@
 import logging
 
 from flask import Flask
-from flask_ask import Ask, request, session, question, statement
+from flask import render_template
+
+from flask_ask import Ask
+# from flask_ask import request
+# from flask_ask import session
+from flask_ask import question
+from flask_ask import statement
 
 
 app = Flask(__name__)
@@ -11,25 +17,24 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
-    speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    speech_text = render_template('welcome')
+    return question(speech_text).reprompt(speech_text)
 
 
-@ask.intent('HelloWorldIntent')
+@ask.intent('StartWorkoutIntent')
 def hello_world():
-    speech_text = 'Hello world'
+    speech_text = render_template('start')
     return statement(speech_text).simple_card('HelloWorld', speech_text)
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
-    speech_text = 'You can say hello to me!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    return launch()
 
 
 @ask.session_ended
 def session_ended():
-    return "", 200
+    return '', 200
 
 
 if __name__ == '__main__':
